70fc885ff18528d41e28d3f529ebec0f11635ec3
Luis de la Cruz <lec_mtez@outlook.com>
2016-10-07 09:13:20 -0500

    Initial commit

---
+++ app/src/main/java/lecmtez/desapp/com/calculadora/calculadora.java
@@ -0,0 +1,13 @@
+package lecmtez.desapp.com.calculadora;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class calculadora extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculadora);
+    }
+}
